9f6e9574f723f34c7a0a39b431ecdc208af0a8b0
meli fernandez sassone <mfernandezsassone@gmail.com>
2019-02-14 11:23:55 -0300

    falta segundo model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Piacere',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '5786-9988',
+    category:     'italian'
+  },
+  {
+    name:         'China East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '4567-8989',
+    category:     'chinese'
+  },
+    {
+    name:         'Je suis',
+    address:      '57A Shoreditch High St, London E1 6PQ',
+    phone_number: '49008-978',
+    category:     'french'
+  },
+    {
+    name:         'Brujas',
+    address:      '59A Shoreditch High St, London E1 6PQ',
+    phone_number: '5678-9090',
+    category:     'belgian'
+  },
+    {
+    name:         'SuChi',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '5674-9890',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
